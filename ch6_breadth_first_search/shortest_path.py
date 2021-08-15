03bef91c1ab7f430b307bde22ca63d4f0a23b0d6
Sofia Zavala <szavala3@outlook.com>
2021-08-15 15:53:50 -0400

    breadth first search

---
+++ ch6_breadth_first_search/shortest_path.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# How do you find the shortest path between two points? Breadth-first search.
+# Breadth first search determines whether there is a path from A to B and
+# what is the shortest path from A to B.
+
+# Translate your data into a graph.
+# Graphs are made of items called nodes and links between nodes are edges.
+# Nodes are represented as circles and edges as arrows (or lines).
+
+# Directed graphs use arrows for edges.
+# An arrow starting at node Sunny Jim and ending at node Dougie Jones means
+# SJ has a direct relationship to DJ. Sunny is linked to the family through Dougie.
+
+# An Undirected graph uses lines for edges.
+# A line means the relationship goes both ways.
+# A line starting at node Nomi and ending at node Amanita means
+# Nomanita is an iconic tv love and also 
+# Nomi is married to Amanitia and Aminita is married to Nomi.
+
+# Once you have a graph how do you navigate it?
+# Use a queue structure. A queue is a collection of elements accessed by FIFO.
